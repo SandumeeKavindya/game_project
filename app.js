63f443f2826d9315e036449e2ebc9cb4907d5bd5
SandumeeKavindya <hewagesandumee@gmail.com>
2024-06-26 15:19:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let randomNumber = Math.floor((Math.random() * 10) + 1);
+console.log(randomNumber);  
+
+
+function valid() {
+    
+    let userInput = parseInt(document.getElementById("addnum").value, 10);
+
+    if (randomNumber === userInput) {
+       document.getElementById("btn").innerHTML="You won!!!!!!!!!!!!!!!!!";
+    } else if (randomNumber < userInput) {
+        document.getElementById("btn").innerHTML="Number is greater than the random number, try again!";
+    } else {
+        document.getElementById("btn").innerHTML="Number is less than the random number, try again!";
+    }
+}
